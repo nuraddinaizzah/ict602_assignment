fcd8fd40dab55239bf846cb5577b7aeeedf2859f
user <ijjahizzah2106@gmail.com>
2024-05-11 22:23:36 +0800

    first commit

--- app/src/main/java/com/example/electricitycalculatorapplication/MainActivity.java
+++ app/src/main/java/com/example/electricitycalculatorapplication/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
 
         editTextUnits = findViewById(R.id.editTextUnits);
         editTextRebate = findViewById(R.id.editTextRebate);
